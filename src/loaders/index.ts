0e2ddb2ede2a1f7a0e81f9fd509d059a48d7ac81
orsel <orselv99@gmail.com>
2021-07-08 23:35:26 +0900

    설정값 환경파일로 통합 및 db 연결

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,18 @@
-import expressLoader from './express'
+import Logger from './logger';
+import DBConnectionLoader from './db';
+import DependencyInjectorLoader from './dependencyInjector';
+import ExpressLoader from './express'
 
-export default async (app: any) => {
+export default async (app) => {
     // db connection
+    const dbConnection = await DBConnectionLoader();
+    Logger.info('🔊 DB connected!');
 
     // dependency injector
+    await DependencyInjectorLoader(dbConnection);
+    Logger.info('🔊 Dependency Injector loaded');
 
     // express 설정
-    expressLoader(app);
+    await ExpressLoader(app);
+    Logger.info('🔊 Express loaded.');
 }
